787a6ed8ea415643edcbf1dced70761010383014
Kanza Shahid <90906602+kanza-shahid-dev@users.noreply.github.com>
2024-01-03 17:57:07 +0500

    vercel Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
